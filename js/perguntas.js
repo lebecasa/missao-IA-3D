558b10a0ae1567288f2706786926617d869e1320
lebecasa <148072673+lebecasa@users.noreply.github.com>
2024-08-21 12:18:28 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,57 @@
+const perguntas = [
+    {
+        enunciado: "O que são mitos?",
+        alternativas: [
+            {
+                texto: "é uma narrativa fantástica que possui o objetivo de explicar a origem de tudo aquilo que existe e é considerado importante para um determinado povo.",
+                afirmacao: [
+                    "de fato povos tradicionais costumavam criar os ditos mitos para explicar tais acontecimentos “inexplicáveis”, pois naqueles tempos a ciência não era um método utilizado frequentemente."
+                    ]
+            },
+            {
+                texto: "são estados mentais em que se assume que algo é verdadeiro ou provável. Elas são expressas linguisticamente por meio de afirmações.",
+                afirmacao: [
+                    "errado essa é a definição de crença."
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Qual a importância do folclore na cultura brasileira?",
+        alternativas: [
+            {
+                texto:"é um meio de aproximação entre os povos e grupos sociais e de afirmação de sua identidade cultural.",
+                afirmacao: [
+                    "Apesar de não ser uma resposta completamente errada, é bom lembrar de que o folclore foi e ainda é"
+                    ]
+            },
+            {
+                texto: "Alternativa 2 da pergunta 2",
+                afirmacao: [
+                    "Simboliza a essência da cultura popular, atuando como um pilar fundamental na construção da identidade de um povo."
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Alternativa 2 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];
